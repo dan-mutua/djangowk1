9e9d3e222c84cd7b19b5c1d168ab824876233086
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-12 22:16:12 +0300

    add photo

--- pblog/maboto/models.py
+++ pblog/maboto/models.py
@@ -5,9 +5,22 @@ from django.urls import reverse
 
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=300)
+
+  def __str__(self):
+      return self.name
+
+  def get_absolute_url(self):
+    return reverse('home')
+
+
+
+
 class Post(models.Model):
   title = models.CharField(max_length=200)
   author = models.ForeignKey(User, on_delete=models.CASCADE)
+  category = models.CharField(max_length=200,default='supercar')
   body = models.TextField()
 
   def __str__(self):
--- pblog/maboto/views.py
+++ pblog/maboto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,UpdateView,DeleteView
+from django.views.generic import ListView,DetailView,UpdateView,DeleteView,CreateView
 from .models import Post
 from .forms import PostForm
 from django.urls import reverse_lazy
@@ -16,6 +16,16 @@ class HomePage(ListView):
 class  BlogD(DetailView):
   model = Post
   template_name='p_detail.html'
+
+class Addp(CreateView):
+  model = Post
+  form_class=PostForm
+  template_name= 'addphoto.html'  
+
+class AddCategory(CreateView):
+  model = Post
+  fields= '__all__'
+  template_name= 'addcategory.html'    
 
 
 class UpdateViewB(UpdateView):
