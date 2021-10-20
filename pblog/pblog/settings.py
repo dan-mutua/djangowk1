cfdf7f7afaaed0e364ae1014ea7a5ba38ad076c2
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-20 16:55:55 +0300

    change settings.py

--- pblog/pblog/settings.py
+++ pblog/pblog/settings.py
@@ -45,7 +45,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'ENGINE': 'django.db.backends.postgresql',
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
